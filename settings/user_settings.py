1c5f7b48147ee41f978f5b20cfc3ca7d59ae17a0
Anonymous <159781143+4n0nymou3@users.noreply.github.com>
2026-09-09 04:22:40 +0330

    user_settings.py

--- settings/user_settings.py
+++ settings/user_settings.py
@@ -56,7 +56,7 @@ ENABLED_PROTOCOLS = {
 
 # Maximum age of configurations in days.
 # Configurations older than this will be considered invalid.
-MAX_CONFIG_AGE_DAYS = 1
+MAX_CONFIG_AGE_DAYS = 3
 
 # --- Xray Config Tester Settings ---
 
